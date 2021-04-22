c890a8982408cd42ab34f037bef6e07bbe66a1b5
Sage Ross <ragesoss@gmail.com>
2021-04-22 10:27:16 -0700

    Update variable annotations and instructions

--- linode/variables.tf
+++ linode/variables.tf
@@ -2,7 +2,7 @@ variable "consul_mgmt_token" {
   type        = string
   description = <<EOT
     Token used to secure Consul - this token grants global permissions to bearer.
-    Use `$ openssl rand 24 -hex` to generate a sufficiently random value.
+    Use `openssl rand -hex 24` to generate a sufficiently random value.
   EOT
   sensitive   = true
 }
@@ -11,7 +11,7 @@ variable "consul_gossip_token" {
   type    = string
   description = <<EOT
     Token used to secure Consul gossip protocol.
-    Use `$ consul keygen` to generate.
+    Use `openssl rand -hex 24` to generate.
   EOT
   sensitive = true
 }
@@ -32,7 +32,7 @@ variable "root_pass" {
   type = string
   description = <<EOT
     All root passwords will be set to this value.
-    Use `$ openssl rand 16 -hex` to generate a sufficiently random value.
+    Use `openssl rand -hex 16` to generate a sufficiently random value.
   EOT
   sensitive = true
 }
@@ -52,5 +52,5 @@ variable "rails_domain" {
 variable "letsencrypt_email" {
   type = string
   description = "Email address for LE certs"
-  default = "nate.berkopec@speedshop.co"
+  default = "sage@wikiedu.org"
 }
